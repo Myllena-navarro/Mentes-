5b4181354e21b233c83b30111ccefe2717add6ed
Mirella Santana <134963126+mihebs@users.noreply.github.com>
2024-11-14 18:56:03 -0300

    Update screen.c

--- src/screen.c
+++ src/screen.c
@@ -1,171 +1,107 @@
 #include "screen.h"
 
-void screenDrawBorders() 
-{
-    char hbc = BOX_HLINE;
-    char vbc = BOX_VLINE;
-    
-    screenClear();
-    screenBoxEnable();
-    
-    screenGotoxy(MINX, MINY);
-    printf("%c", BOX_UPLEFT);
+void screenDrawBorders() {
+  char hbc = BOX_HLINE;
+  char vbc = BOX_VLINE;
 
-    for (int i=MINX+1; i<MAXX; i++)
-    {
-        screenGotoxy(i, MINY);
-        printf("%c", hbc);
-    }
-    screenGotoxy(MAXX, MINY);
-    printf("%c", BOX_UPRIGHT);
+  screenBoxEnable(); 
 
-    for (int i=MINY+1; i<MAXY; i++)
-    {
-        screenGotoxy(MINX, i);
-        printf("%c", vbc);
-        screenGotoxy(MAXX, i);
-        printf("%c", vbc);
-    }
+  screenGotoxy(MINX, MINY);
+  printf("%c", BOX_UPLEFT);
 
-    screenGotoxy(MINX, MAXY);
-    printf("%c", BOX_DWNLEFT);
-    for (int i=MINX+1; i<MAXX; i++)
-    {
-        screenGotoxy(i, MAXY);
-        printf("%c", hbc);
-    }
-    screenGotoxy(MAXX, MAXY);
-    printf("%c", BOX_DWNRIGHT);
+  for (int i = MINX + 1; i < MAXX; i++) {
+    screenGotoxy(i, MINY);
+    printf("%c", hbc);
+  }
 
-    screenBoxDisable();
-    
+  screenGotoxy(MAXX, MINY);
+  printf("%c", BOX_UPRIGHT);
+
+  for (int i = MINY + 1; i < MAXY; i++) {
+    screenGotoxy(MINX, i);
+    printf("%c", vbc);
+    screenGotoxy(MAXX, i);
+    printf("%c", vbc);
+  }
+
+  screenGotoxy(MINX, MAXY);
+  printf("%c", BOX_DWNLEFT);
+  for (int i = MINX + 1; i < MAXX; i++) {
+    screenGotoxy(i, MAXY);
+    printf("%c", hbc);
+  }
+
+  screenGotoxy(MAXX, MAXY);
+  printf("%c", BOX_DWNRIGHT);
+
+  screenBoxDisable();  
 }
 
-void screenInit(int drawBorders)
-{
-    screenClear();
-    if (drawBorders) screenDrawBorders();
-    screenHomeCursor();
-    screenHideCursor();
+void screenInit(int drawBorders) {
+  screenClear();
+  if (drawBorders) 
+    screenDrawBorders();
 }
 
-void screenDestroy()
-{
-    printf("%s[0;39;49m", ESC); 
-    screenSetNormal();
-    screenClear();
-    screenHomeCursor();
-    screenShowCursor();
+void screenDestroy() {
+
 }
 
-void screenGotoxy(int x, int y)
-{
-    x = ( x<0 ? 0 : x>=MAXX ? MAXX-1 : x);
-    y = ( y<0 ? 0 : y>MAXY ? MAXY : y);
-    
-    printf("%s[f%s[%dB%s[%dC", ESC, ESC, y, ESC, x);
+void screenSetColor(screenColor fg, screenColor bg) {
+  char atr[] = "[0;";
+
+  if (fg > LIGHTGRAY) {
+    atr[1] = '1';
+    fg -= 8;
+  }
+
+  printf("%s%s%d;%dm", ESC, atr, fg + 30, bg + 40);
 }
 
-void screenSetColor( screenColor fg, screenColor bg)
-{
-    char atr[] = "[0;";
-
-    if ( fg > LIGHTGRAY )
-    {
-        atr[1] = '1';
-		fg -= 8;
-    }
-
-    printf("%s%s%d;%dm", ESC, atr, fg+30, bg+40);
+void screenForca(int erros) {
+      screenGotoxy(FORCA_X, FORCA_Y); 
+      printf("%c%c%c", BOX_UPLEFT, BOX_HLINE, BOX_UPRIGHT);
+      screenGotoxy(FORCA_X, FORCA_Y + 1);
+      printf("%c%c%c", BOX_VLINE, ' ', BOX_VLINE); 
+      screenGotoxy(FORCA_X, FORCA_Y + 2);
+      printf("%c%c%c", BOX_VLINE, ' ', BOX_VLINE); 
+      screenGotoxy(FORCA_X + 2, FORCA_Y + 2);      
+      printf("%c", BOX_HLINE);                   
 }
 
-void screenShowTime(int time) {
-    char timeMessage[20];
-    sprintf(timeMessage, "Tempo restante: %d segundos", time);
+void screenTrofeu() {
+      screenGotoxy(GANHAR_TROFEU_X,
+                   GANHAR_TROFEU_Y); 
+      printf("%c%c%c", BOX_UPLEFT, BOX_HLINE,
+             BOX_UPRIGHT); 
+      screenGotoxy(GANHAR_TROFEU_X, GANHAR_TROFEU_Y + 1);
+      printf("%c%c%c", BOX_VLINE, ' ', BOX_VLINE); 
+      screenGotoxy(GANHAR_TROFEU_X, GANHAR_TROFEU_Y + 2);
+      printf("%c%c%c", BOX_VLINE, ' ', BOX_VLINE); 
+      screenGotoxy(GANHAR_TROFEU_X + 2, GANHAR_TROFEU_Y + 3);
+      printf("%c", BOX_HLINE); 
+}
 
-    int msgLen = strlen(timeMessage);
-
-    int msgX = (MAXX - msgLen) / 2;
-    int msgY = MAXY - 2;
-    screenGotoxy(centerX, centerY);
-
-    printf("%s", timeMessage);
+void screenCaveira() {
+      screenGotoxy(PERDER_CAVEIRA_X,
+                   PERDER_CAVEIRA_Y); 
+      printf("%c%c%c", BOX_UPLEFT, BOX_HLINE,
+             BOX_UPRIGHT); 
+      screenGotoxy(PERDER_CAVEIRA_X, PERDER_CAVEIRA_Y + 1);
+      printf("%c%c%c", BOX_VLINE, ' ', BOX_VLINE); 
+      screenGotoxy(PERDER_CAVEIRA_X + 2, PERDER_CAVEIRA_Y + 2);
+      printf("%c", BOX_CROSS); 
 }
 
 
-void screenWelcome() {
-    char welcomeMessage[] = "Bem-vindo ao jogo da Forca!!";
-    int msgLen = strlen(welcomeMessage);
-    
-    int msgX = (MAXX - msgLen) / 2;
-    int msgY = MAXY / 2;
-    
-    screenGotoxy(msgX, msgY);
-    printf("%s", welcomeMessage);
+void screenGotoxy(int x, int y) {
+  
 }
 
-void screenDrawHangman(int errors) {
-    char hangman[] = 
-        "  ________\n"
-        "  |      |\n"
-        "  |      %c%c%c\n"
-        "  |      %c%c%c\n"
-        "  |      %c\n"
-        "  |      %c%c\n"
-        "  |          \n"
-        " _|___       \n";
+void screenBoxDisable() {
 
-    printf(hangman, 
-           (errors >= 1 ? '(' : ' '), 
-           (errors >= 1 ? '_' : ' '), 
-           (errors >= 1 ? ')' : ' '),
-           (errors >= 3 ? '\\' : ' '), 
-           (errors >= 2 ? '|' : ' '), 
-           (errors >= 3 ? '/' : ' '), 
-           (errors >= 2 ? '|' : ' '), 
-           (errors >= 4 ? '/' : ' '), 
-           (errors > 4 ? '\\' : ' '));
 }
 
-void screenDrawTrophy() {
-    char trophyMessage[] = "Parabéns, você acertou a palavra!";
-    char trophy[] = 
-        "     ___________   \n"
-        "    '._==_==_=_.'  \n"
-        "    .-\\:      /-. \n"
-        "   | (|:.     |) | \n"
-        "    '-|:.     |-'  \n"  
-        "      \\::.    /   \n"
-        "       '::. .'     \n"
-        "         ) (       \n"  
-        "       _.' '._     \n"
-        "       '-----'     \n\n"; 
-
-    printf("%s\n", trophyMessage);
-    printf("%s", trophy);
-}
-
-void screenDrawGameOver() {
-    char gameOverMessage[] = "Você perdeu! A palavra era: ";
-    char hangman[] = 
-        "   _______________         \n"
-        "  /               \\       \n"
-        " /                  \\     \n"
-        "//                   \\    \n"
-        " |   ----     ----   | /   \n"
-        " |   ----     ----   |/    \n"
-        " |    ---     ---    |     \n"
-        " |                   |     \n"
-        " \\__     ---     __/      \n"
-        "   |\\    ---     /|       \n"
-        "   | |           | |       \n"
-        "   | X X X X X X X |       \n"   
-        "   |  X X X X X X  |       \n"
-        "   \\_            _/       \n"
-        "     \\_         _/        \n"
-        "       \\_______/          \n";
-
-    printf("%s%s\n", gameOverMessage);
-
-    printf("%s", hangman);
+void screenUpdate(int erros) {
+    screenForca(erros); 
 }
