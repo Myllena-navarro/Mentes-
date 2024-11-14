82027bc4f1b022894b434558fcee358ba472d6c6
Mirella Santana <134963126+mihebs@users.noreply.github.com>
2024-11-14 18:56:34 -0300

    Update timer.c

--- src/timer.c
+++ src/timer.c
@@ -1,48 +1,13 @@
 #include "timer.h"
-#include <sys/time.h>
-#include <stdio.h>
 
-static struct timeval timer, now;
-static int delay = -1;
+static time_t startTime;
+static int maxTime;
 
-void timerInit(int valueMilliSec)
-{
-    delay = valueMilliSec;
-    gettimeofday(&timer, NULL);
+void timerInit(int tempoMax) {
+    startTime = time(NULL);  
+    maxTime = tempoMax;  
 }
 
-void timerDestroy()
-{
-    delay = -1;
-}
-
-void timerUpdateTimer(int valueMilliSec)
-{
-    delay = valueMilliSec;
-    gettimeofday(&timer, NULL);
-}
-
-int getTimeDiff()
-{
-    gettimeofday(&now, NULL);
-    long diff = (((now.tv_sec - timer.tv_sec) * 1000000) + now.tv_usec - timer.tv_usec)/1000;
-    return (int) diff;
-}
-
-int timerTimeOver()
-{
-    int ret = 0;
-
-    if (getTimeDiff() > delay)
-    {
-        ret = 1;
-        gettimeofday(&timer, NULL);
-    }
-
-    return ret;
-}
-
-void timerPrint()
-{
-    printf("Timer:  %d", getTimeDiff());
+int timerTimeOver(void) {
+    return difftime(time(NULL), startTime) >= maxTime;
 }
